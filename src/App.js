bd227bd5b5224a7b653d1f9e8501bfb3fc604f0c
emreeken183425 <emreeken1834@gmail.com>
2022-11-05 15:55:34 +0300

    merhaba

--- src/App.js
+++ src/App.js
@@ -6,10 +6,25 @@ import Card from "./components/Card";
 
 function App() {
   return (
-    <div className="app">
-         <Card cardTitle="Beşiktaş" />
-         <Card cardTitle="erzurum" />
-         <Card cardTitle="aşkale" />
+    <div className="app d-flex m-auto ">
+         <Card cardTitle="Card title 1" 
+         cardText="lorem ipsum 1" 
+         updatedTime="last uptated 1 min ago "
+         image="https://picsum.photos/id/23/200/300"
+         />
+
+         <Card cardTitle="Card title 2" 
+         cardText="lorem ipsum 2"
+         updatedTime="last uptated 1 min ago "
+         image="https://picsum.photos/id/10/200/300"
+         />
+
+         <Card cardTitle="Card title 3"  
+         cardText="lorem ipsum 3" 
+         updatedTime="last uptated 1 min ago "
+         image="https://picsum.photos/id/235/200/300"
+         />
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,18 +6,17 @@ const Card = (props) => {
   return (
     
     
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
+        <div className="card d-flex ">
+          <img src={props.image} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{props.cardTitle} </h5>
             <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              <small className="text-muted">{props.cardText} </small>
             </p>
             <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
+              {props.updatedTime }
             </p>
+          
           </div>
         </div>
         
